d5e1718635c40e30d99b44f3bcf662a629ee23dc
Chinaka Chinedu <143916594+Neddie28@users.noreply.github.com>
2024-08-15 00:56:35 +0100

    Update use-chat-query.ts

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -1,6 +1,5 @@
 import qs from "query-string";
 import { useInfiniteQuery } from "@tanstack/react-query";
-
 import { useSocket } from "@/components/providers/socket-provider";
 
 interface ChatQueryProps {
